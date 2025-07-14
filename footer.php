2f004a83433c7fc8721bf02ea50a9b847297504d
christinagmb <christinaventura2003@gmail.com>
2025-07-15 00:40:10 +0800

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,10 @@
+<footer style="background-color: white; padding: 20px 0; text-align: center;">
+  <p style="margin: 0; font-family: Poppins; color: #524a48; font-weight: 500;">
+    &copy; 2025 Christina Gomba. All rights reserved.
+  </p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
+
+</body>
+</html>
